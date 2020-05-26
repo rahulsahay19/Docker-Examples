a111a16be6474f12cb6dc9fa8d390f68871ef9f5
Rahul Sahay <rahulsahay19@hotmail.com>
2020-05-26 23:42:33 +0530

    initial commit

---
+++ console-app-input/console-app-input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_app_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Input");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Input entered as: {name}");
+        }
+    }
+}
+
---
+++ hello-docker-sample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hello_docker_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Getting started with docker!");
+            }
+            else
+            {
+                Console.WriteLine("Printing arguments!");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+            }
+        }
+    }
+}
